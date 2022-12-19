3afa85d82368f653214701f86156dc1d9a94cea6
ppedvLukasK <lukask@ppedv.de>
2022-12-19 14:40:38 +0100

    M004

---
+++ Generics/Constraints.cs
@@ -0,0 +1,43 @@
+﻿namespace Generics;
+
+internal class Constraints
+{
+	public Constraints(int x) { } //Parameterloser Konstruktor wird überschrieben
+
+	public Constraints() { }
+
+	static void Main(string[] args)
+	{
+		DataStore4<Constraints> d;
+		DataStore5<DayOfWeek> d1;
+	}
+
+	public class DataStore1<T> where T : class { } //T muss ein Referenztyp sein
+
+	public class DataStore2<T> where T : struct { } //T muss ein Wertetyp sein
+
+	public class DataStore3<T> where T : Program { } //T muss die Klasse selber oder eine Unterklasse sein
+
+	public class DataStore4<T> where T : new() { } //T muss einen Standardkonstruktor
+
+	public class DataStore5<T> where T : Enum { } //Nur Enums (keine Enumwerte)
+
+	public class DataStore6<T> where T : Delegate { } //Nur Delegates (Action, Func, eigenes Delegate, ...)
+
+	public class DataStore7<T> where T : unmanaged { } //Nur Basisdatentypen und ein paar weitere Typen
+	//https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/unmanaged-types
+
+	public class DataStore8<T> where T : class, new() { } //Mehrere Constraints auf ein Generic
+
+	public class DataStore9<T1, T2> //Mehrere Constraints auf mehrere Generics
+		where T1 : class
+		where T2 : struct
+	{ }
+
+	public class DataStore10<T1, T2, T3, T4, T5, T6> { } //Beliebig viele Generics möglich
+
+	public void Test<T>() where T : unmanaged //Constrains bei Methode hinzufügen
+	{
+
+	}
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Generics;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		List<string> list = new List<string>(); //Generic: T wird nach unten übernommen (hier T = string)
+		list.Add("123"); //T wird durch string ersetzt Add(T) -> Add(string)
+
+		List<int> ints = new List<int>(); //T wird durch int ersetzt
+		ints.Add(1);
+
+		Dictionary<string, int> dict = new Dictionary<string, int>(); //Klasse mit 2 Generics: TKey -> string, TValue -> int
+		dict.Add("123", 1);
+	}
+}
+
+public class DataStore<T>
+	: IProgress<T>, //T bei Vererbung weitergeben
+	  IEquatable<int> //Fixer Typ statt T
+{
+	private T[] data; //T als Typ
+
+	public List<T> Data => data.ToList(); //Generic wird nach unten weitergegeben
+
+	public void Add(int index, T item) //T als Parameter
+	{
+		data[index] = item;
+	}
+
+	public T GetIndex(int i) //T als Rückgabewert
+	{
+		if (i < 0 || i > data.Length)
+			return default(T); //default(T): Standardwert von T (int: 0, string: null, bool: false, ...)
+		return data[i];
+	}
+
+	public void Report(T value) //T kommt von Interface
+	{
+		throw new NotImplementedException();
+	}
+
+	public bool Equals(int other)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void PrintType<MyType>() //Methode mit Generic, T von oben hier nicht nochmal definieren
+	{
+		Console.WriteLine(typeof(MyType)); //Typ Objekt des Typs generieren
+		Console.WriteLine(nameof(MyType)); //Gibt den Namen des Typs aus ("int", "string", "bool", ...)
+		Console.WriteLine(default(MyType)); //default(T): Standardwert von T (int: 0, string: null, bool: false, ...)
+		
+		//if (MyType is int) { } //Nicht möglich
+
+		if (typeof(MyType) == typeof(int)) //Typvergleich muss so gemacht werden
+		{
+
+		}
+	}
+}
+
+public class DataStore2<T> : DataStore<T> { } //Klassen mit T vererben: braucht wieder T beim Klassennamen
