6b92330b5ef474e70eedac8cb3bb2d89a48d67da
ppedvLukasK <lukask@ppedv.de>
2022-12-19 10:08:55 +0100

    M001

---
+++ Sprachfeatures/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Sprachfeatures
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string name = "lukas";
+			string nameFix = char.ToUpper(name[0]) + name[1..].ToLower();
+
+			Program p = null;
+		}
+	}
+}
