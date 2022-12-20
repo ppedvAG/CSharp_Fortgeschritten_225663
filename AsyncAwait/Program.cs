e11aed5a9ac545183b2a16f433b12b27e47ef587
ppedvLukasK <lukask@ppedv.de>
2022-12-20 16:32:36 +0100

    M008

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,109 @@
+﻿using System.Diagnostics;
+
+namespace AsyncAwait;
+
+internal class Program
+{
+	static async Task Main(string[] args)
+	{
+		//Stopwatch sw = Stopwatch.StartNew(); //Sequentiell, ineffizient
+		//Toast();
+		//Geschirr();
+		//Kaffee();
+		//sw.Stop();
+		//Console.WriteLine(sw.ElapsedMilliseconds); //7s
+
+		//Stopwatch sw2 = Stopwatch.StartNew();
+		//Task.Run(Toast);
+		//Task.Run(Geschirr);
+		//Task.Run(Kaffee);
+		//sw2.Stop();
+		//Console.WriteLine(sw2.ElapsedMilliseconds); //24ms, Main Thread läuft weiter
+		//Console.ReadKey();
+
+		//Stopwatch sw3 = Stopwatch.StartNew();
+		//ToastTaskAsync(); //Methoden werden als Tasks ausgeführt weil sie async sind
+		//GeschirrTaskAsync();
+		//KaffeeTaskAsync();
+		//sw3.Stop();
+		//Console.WriteLine(sw3.ElapsedMilliseconds); //23ms, Main Thread läuft weiter
+
+		Stopwatch sw4 = Stopwatch.StartNew();
+		Task<Toast> toastTask = ToastAsync();
+		Task<Tasse> tasseTask = GeschirrAsync();
+		Tasse tasse = await tasseTask; //Alternative zu t.Result (die nicht blockiert)
+		Task<Kaffee> kaffeeTask = KaffeeAsync(tasse);
+		Kaffee k = await kaffeeTask; //Warte hier bis der Kaffee fertig ist (alles läuft im Hintergrund weiter)
+		Toast t = await toastTask;
+		sw4.Stop();
+		Console.WriteLine(sw4.ElapsedMilliseconds); //4s
+
+		//Kurzform
+		Tasse t3 = await GeschirrAsync();
+		Kaffee k2 = await KaffeeAsync(t3);
+		Toast t2 = await ToastAsync();
+	}
+
+	static void Toast()
+	{
+		Thread.Sleep(4000);
+		Console.WriteLine("Toast fertig");
+	}
+
+	static void Geschirr()
+	{
+		Thread.Sleep(1500);
+		Console.WriteLine("Geschirr hergerrichtet");
+	}
+
+	static void Kaffee()
+	{
+		Thread.Sleep(1500);
+		Console.WriteLine("Kaffee fertig");
+	}
+
+	static async void ToastTaskAsync()
+	{
+		await Task.Delay(4000);
+		Console.WriteLine("Toast fertig");
+	}
+
+	static async void GeschirrTaskAsync()
+	{
+		await Task.Delay(1500);
+		Console.WriteLine("Geschirr hergerrichtet");
+	}
+
+	static async void KaffeeTaskAsync()
+	{
+		await Task.Delay(1500);
+		Console.WriteLine("Kaffee fertig");
+	}
+
+	static async Task<Toast> ToastAsync()
+	{
+		await Task.Delay(4000);
+		Console.WriteLine("Toast fertig");
+		return new Toast();
+	}
+
+	static async Task<Tasse> GeschirrAsync()
+	{
+		await Task.Delay(1500);
+		Console.WriteLine("Geschirr hergerrichtet");
+		return new Tasse();
+	}
+
+	static async Task<Kaffee> KaffeeAsync(Tasse t)
+	{
+		await Task.Delay(1500);
+		Console.WriteLine("Kaffee fertig");
+		return new Kaffee();
+	}
+}
+
+public class Toast { }
+
+public class Tasse { }
+
+public class Kaffee { }
---
+++ AsyncAwaitWPF/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AsyncAwaitWPF;
+
+public partial class MainWindow : Window
+{
+	public MainWindow() => InitializeComponent();
+
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		Progress.Value = 0;
+		for (int i = 0; i < 100; i++)
+		{
+			Thread.Sleep(25);
+			Progress.Value++;
+		} //Thread.Sleep blockiert den UI Thread
+	}
+
+	private void Button_Click_1(object sender, RoutedEventArgs e)
+	{
+		Task.Run(() => //UI Updates von Side Tasks sind nicht erlaubt
+		{
+			Dispatcher.Invoke(() => Progress.Value = 0); //Dispatcher.Invoke um UI Updates auf dem Main Thread auszuführen
+			for (int i = 0; i < 100; i++)
+			{
+				Thread.Sleep(25);
+				Dispatcher.Invoke(() => Progress.Value++);
+			}
+		});
+	}
+
+	private async void Button_Click_2(object sender, RoutedEventArgs e)
+	{
+		Progress.Value = 0;
+		for (int i = 0; i < 100; i++)
+		{
+			await Task.Delay(25);
+			Progress.Value++;
+		}
+	}
+
+	private async void Button_Click_3(object sender, RoutedEventArgs e)
+	{
+		Progress.Value = 0;
+		Progress.Maximum = 2;
+
+		using HttpClient client = new();
+		Task<HttpResponseMessage> respTask = client.GetAsync("http://www.gutenberg.org/files/54700/54700-0.txt");
+		//UI Update: Text lädt...
+		Text.Text = "Text lädt...";
+		HttpResponseMessage resp = await respTask;
+
+		Task<string> readTask = resp.Content.ReadAsStringAsync();
+		//UI Update: Text wird ausgelesen...
+		Progress.Value++;
+		Text.Text = "Text wird ausgelesen";
+		await Task.Delay(1000);
+		string s = await readTask;
+
+		Progress.Value++;
+		Text.Text = s;
+	}
+
+	private async void Button_Click_4(object sender, RoutedEventArgs e)
+	{
+		List<int> ints = Enumerable.Range(0, 100).ToList();
+		await Parallel.ForEachAsync(ints, (item, ct) => 
+		{
+			Console.WriteLine(item * 10);
+			return ValueTask.CompletedTask;
+		});
+
+		IAsyncEnumerable<int> liste = null;
+		await foreach (int i in liste)
+		{
+
+		}
+	}
+}
