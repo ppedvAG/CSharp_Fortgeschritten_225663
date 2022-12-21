6d291031d3f500ce4dfe778f5c348c4eba7da729
ppedvLukasK <lukask@ppedv.de>
2022-12-21 16:27:07 +0100

    M013

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -7,5 +7,7 @@ internal class Program
 	static void Main(string[] args)
 	{
 		BenchmarkRunner.Run<Benchmarks>();
+		//BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, new DebugInProcessConfig());
+		//Zum Debuggen auf Debug umschalten, obere Zeile auskommentieren
 	}
 }
